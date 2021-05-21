9eedc2e6324e948e59ca21e1d7285c35ad70bacb
Laurie Waller <lauriewaller7@gmail.com>
2021-05-20 23:07:26 -0700

    add Index and Create route methods for Clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
@@ -11,7 +11,7 @@ namespace HairSalon.Controllers
   {
     private readonly HairSalonContext _db;
 
-    public ClientsController (HairSalonContext db)
+    public ClientsController(HairSalonContext db)
     {
       _db = db;
     }
@@ -20,6 +20,20 @@ namespace HairSalon.Controllers
     {
       List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
